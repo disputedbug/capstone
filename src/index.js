e409906e531d089e952e9d3d932cfa363146301f
Chetan Sharma <Chetan.Sharma@cerner.com>
2023-11-13 20:25:39 +0530

    Add: Implement NGO-Volunteer endpoint

--- src/index.js
+++ src/index.js
@@ -9,6 +9,44 @@ app.get('/', (req, res) => {
   res.send("Node js running on your machine now");
 });
 
+app.get('/find/vol-ngo', async (req, res) => {
+    const client = await connectToDb();
+    // Specify the database and collection
+    const database = client.db('Volunteer-Match');
+    const volunteerColl = database.collection('Volunteer');
+    const volDocs = await volunteerColl.find({});
+
+    const volunteers = [];
+    for await (const doc of volDocs) {
+        volunteers.push(doc);
+    }
+
+    const ngoColl = database.collection('NGO');
+    const ngoDocs = await ngoColl.find({});
+
+    const ngos = [];
+    for await (const doc of ngoDocs) {
+        ngos.push(doc);
+    }
+
+    const result = [];
+
+    ngos.forEach(ngo => {
+        const { FieldOfInterest: ngoField, VolunteerDays: ngoDay } = ngo;
+        const volunteerResult = [];
+        volunteers.forEach(volunteer => {
+            const { FieldOfInterest: volField, VolunteerDays: volDay } = volunteer;
+            if(ngoField === volField && ngoDay === volDay) volunteerResult.push(volunteer)
+        })
+        result.push({
+            ...ngo,
+            volunteers: volunteerResult
+        })
+    })
+
+    res.send(result);
+});
+
 app.post("/add/volunteer", async (req, res) => {
     const { body } = req;
     console.log(body);
