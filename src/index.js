542f744fca41e79a2c38c058b32b6ccea98452e3
Chetan Sharma <Chetan.Sharma@cerner.com>
2023-11-13 16:48:48 +0530

    DB URI, post

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send("Node js running on your machine now");
+});
+
+app.post("/add/volunteer", (req, res) => {
+
+});
+
+app.post("/add/ngo", (req, res) => {
+    const { body } = req;
+    console.log(body);
+    res.send(body);
 });
 
 app.listen(8080, () => {
