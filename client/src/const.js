d40d09c1b5a374834e3ec92de0b2223f97ae54e9
Himanshu Sharma <disputedbug@gmail.com>
2023-11-14 01:57:27 +0530

    Fix: server url

--- client/src/const.js
+++ client/src/const.js
@@ -1,4 +1,4 @@
-const AWS_URL = "http://http://ec2-13-112-244-109.ap-northeast-1.compute.amazonaws.com:8080/";
+const AWS_URL = "http://ec2-13-112-244-109.ap-northeast-1.compute.amazonaws.com:8080/";
 
 export {
     AWS_URL
