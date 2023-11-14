ab723f98d0aee109938daf1a7a2ccdfb4b018bd5
Himanshu Sharma <disputedbug@gmail.com>
2023-11-14 11:20:31 +0530

    Update: EC2 instance

--- client/src/const.js
+++ client/src/const.js
@@ -1,4 +1,4 @@
-const AWS_URL = "https://ec2-13-112-244-109.ap-northeast-1.compute.amazonaws.com:4000/";
+const AWS_URL = "https://ec2-52-199-220-37.ap-northeast-1.compute.amazonaws.com:8080";
 
 export {
     AWS_URL
