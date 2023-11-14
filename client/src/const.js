35c22d2f6d18940b037e9613754bec84cb57391b
Himanshu Sharma <disputedbug@gmail.com>
2023-11-14 12:02:41 +0530

    Changing the host

--- client/src/const.js
+++ client/src/const.js
@@ -1,4 +1,5 @@
-const AWS_URL = "https://ec2-52-199-220-37.ap-northeast-1.compute.amazonaws.com:8080";
+//const AWS_URL = "https://ec2-52-199-220-37.ap-northeast-1.compute.amazonaws.com:8080";
+const AWS_URL = "https://localhost:8080";
 
 export {
     AWS_URL
